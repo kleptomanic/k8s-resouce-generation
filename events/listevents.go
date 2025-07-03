56a8eb5896f839292771511a842f1b40cacc5da0
Kaushik Raj Panta <krp@logpoint.com>
2025-07-03 14:11:42 +0545

    Slice Return for Event and some comments are added.

--- events/listevents.go
+++ events/listevents.go
@@ -10,15 +10,17 @@ import (
 	typev1 "k8s.io/client-go/kubernetes/typed/core/v1"
 )
 
+// Create Event Connection Interface for the namespace as provided.
 func eventInterface(con *kubernetes.Clientset, namespace string) typev1.EventInterface {
 	return con.CoreV1().Events(namespace)
 }
 
-func ListEvent(con *kubernetes.Clientset, ctx context.Context, namespace string) (*corev1.EventList, error) {
+// Returns the slices of Events
+func ListEvent(con *kubernetes.Clientset, ctx context.Context, namespace string) ([]corev1.Event, error) {
 	eventinterface := eventInterface(con, namespace)
 	getEventList, err := eventinterface.List(ctx, metav1.ListOptions{})
 	if err != nil {
 		return nil, fmt.Errorf("%v", err)
 	}
-	return getEventList, nil
+	return getEventList.Items, nil
 }
