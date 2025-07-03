f5a3f34a859eb2973f6f6915332795735dc7c1f6
Kaushik Raj Panta <krp@logpoint.com>
2025-07-03 16:24:27 +0545

    Job Resource - List Function is added.

---
+++ jobs/listjobs.go
@@ -0,0 +1,24 @@
+package jobs
+
+import (
+	"context"
+	"fmt"
+
+	corev1 "k8s.io/api/batch/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	typev1 "k8s.io/client-go/kubernetes/typed/batch/v1"
+)
+
+func jobsInterface(connection *kubernetes.Clientset, namespace string) typev1.JobInterface {
+	return connection.BatchV1().Jobs(namespace)
+}
+
+func ListJobs(connection *kubernetes.Clientset, ctx context.Context, namespace string) ([]corev1.Job, error) {
+	listinterface := jobsInterface(connection, namespace)
+	jobs, err := listinterface.List(ctx, metav1.ListOptions{})
+	if err != nil {
+		return nil, fmt.Errorf("%v", err)
+	}
+	return jobs.Items, nil
+}
