a43df7d9a29b3af4d8ca717f0eedbd9ddc567761
Kaushik Raj Panta <krp@logpoint.com>
2025-07-04 12:24:30 +0545

    Resize Policy is addded

--- jobs/createjobs.go
+++ jobs/createjobs.go
@@ -38,7 +38,7 @@ func NewJobObject(name string, namespace string) Job {
 								Command: []string{"ping", "-c 4", "google.com"},
 							},
 						},
-						RestartPolicy: RestartPolicyOnFailure,
+						RestartPolicy: corev1.RestartPolicyAlways,
 					},
 				},
 			},
